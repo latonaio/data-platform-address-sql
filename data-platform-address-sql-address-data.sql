b91078417dfb239d5169f42874fc1b263a1cdce8
Yurino Oga <yurino.o@latona.io>
2022-10-16 15:10:36 +0900

    release

--- data-platform-address-sql-address-data.sql
+++ data-platform-address-sql-address-data.sql
@@ -3,13 +3,13 @@ CREATE TABLE `data_platform_address_address_data`
     `AddressID`             int(10) NOT NULL,
     `ValidityEndDate`       varchar(80) NOT NULL,
     `ValidityStartDate`     varchar(80) DEFAULT NULL,
+    `PostalCode`            varchar(10) DEFAULT NULL,
+    `LocalRegion`           varchar(3) DEFAULT NULL,    -- 名称変更
     `Country`               varchar(3) DEFAULT NULL,
-    `Region`                varchar(3) DEFAULT NULL,
-    `District`              varchar(6) DEFAULT NULL,    --新規追加
+    `GlobalRegion`          varchar(3) DEFAULT NULL,    -- 新規追加
+    `District`              varchar(6) DEFAULT NULL,    -- 新規追加
     `StreetName`            varchar(200) DEFAULT NULL,
     `CityName`              varchar(200) DEFAULT NULL,
-    `PostalCode`            varchar(10) DEFAULT NULL,
-    `Language`              varchar(2) DEFAULT NULL,
     PRIMARY KEY (`AddressID`, `ValidityEndDate`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
