fc8e94be1386199e1ea2966419938a4eea1336f0
MiyuYokoyama0109 <miyu.y@latona.io>
2022-10-21 19:21:14 +0900

    release

--- data-platform-address-sql-address-data.sql
+++ data-platform-address-sql-address-data.sql
@@ -13,6 +13,8 @@ CREATE TABLE `data_platform_address_address_data`
     `Builiding`             varchar(100) DEFAULT NULL,  -- 新規追加
     `Floor`                 int(4) DEFAULT NULL,        -- 新規追加
     `Room`                  int(8) DEFAULT NULL,        -- 新規追加
-    PRIMARY KEY (`AddressID`, `ValidityEndDate`)
+    PRIMARY KEY (`AddressID`, `ValidityEndDate`),
+    CONSTRAINT `DataPlatformAddressCountryCountryData_fk` FOREIGN KEY (`Country`) REFERENCES `data_platform_country_country_data` (`Country`),
+    CONSTRAINT `DataPlatformAddressPostalCodePostalCodeData_fk` FOREIGN KEY (`PostalCode`) REFERENCES `data_platform_postal_code_postal_code_data` (`PostalCode`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
