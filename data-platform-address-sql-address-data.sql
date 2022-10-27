18ff672a1b46b8609845524f6a8254d0f35f8e82
MiyuYokoyama0109 <miyu.y@latona.io>
2022-10-27 11:51:31 +0900

    release

--- data-platform-address-sql-address-data.sql
+++ data-platform-address-sql-address-data.sql
@@ -1,20 +1,24 @@
 CREATE TABLE `data_platform_address_address_data`
 (
-    `AddressID`             int(10) NOT NULL,
-    `ValidityEndDate`       varchar(80) NOT NULL,
-    `ValidityStartDate`     varchar(80) DEFAULT NULL,
-    `PostalCode`            varchar(10) DEFAULT NULL,
-    `LocalRegion`           varchar(3) DEFAULT NULL,    -- 名称変更
-    `Country`               varchar(3) DEFAULT NULL,
-    `GlobalRegion`          varchar(3) DEFAULT NULL,    -- 新規追加
+    `AddressID`             int(12) NOT NULL,
+    `ValidityEndDate`       date NOT NULL,
+    `ValidityStartDate`     date NOT NULL,
+    `PostalCode`            varchar(10) NOT NULL,
+    `LocalRegion`           varchar(3) NOT NULL,        -- 名称変更
+    `Country`               varchar(3) NOT NULL,
+    `GlobalRegion`          varchar(3) NOT NULL,        -- 新規追加
+    `TimeZone`              varchar(3) NOT NULL,        -- 新規追加
     `District`              varchar(6) DEFAULT NULL,    -- 新規追加
-    `StreetName`            varchar(200) DEFAULT NULL,
-    `CityName`              varchar(200) DEFAULT NULL,
+    `StreetName`            varchar(200) NOT NULL,
+    `CityName`              varchar(200) NOT NULL,
     `Builiding`             varchar(100) DEFAULT NULL,  -- 新規追加
     `Floor`                 int(4) DEFAULT NULL,        -- 新規追加
     `Room`                  int(8) DEFAULT NULL,        -- 新規追加
+    
     PRIMARY KEY (`AddressID`, `ValidityEndDate`),
-    CONSTRAINT `DataPlatformAddressCountryCountryData_fk` FOREIGN KEY (`Country`) REFERENCES `data_platform_country_country_data` (`Country`),
-    CONSTRAINT `DataPlatformAddressPostalCodePostalCodeData_fk` FOREIGN KEY (`PostalCode`) REFERENCES `data_platform_postal_code_postal_code_data` (`PostalCode`)
+
+    CONSTRAINT `DataPlatformAddressAddressDataPostalCode_fk` FOREIGN KEY (`PostalCode`, `LocalRegion`, `Country`, `GlobalRegion`, `TimeZone`) REFERENCES `data_platform_postal_code_postal_code_data` (`PostalCode`, `LocalRegion`, `Country`, `GlobalRegion`, `TimeZone`),
+    CONSTRAINT `DataPlatformAddressAddressDataDistrict_fk` FOREIGN KEY (`District`, `Country`) REFERENCES `data_platform_district_district_data` (`District`, `Country`)
+    
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
